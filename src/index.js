d885e0f1bde757f21c036ab922dee0e4ba3e33b4
Thierno Sadou Diallo <thierved01@gmail.com>
2018-07-22 22:16:04 +0000

    Added the actions and reducers and setted up the store

--- src/index.js
+++ src/index.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
-import { createStore } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
+//import ReduxPromise from 'redux-promise';
 
 import App from './components/App';
 import reducers from './reducers';
 
+//const store = applyMiddleware(ReduxPromise)(createStore);
 const store = createStore(reducers);
 
 ReactDOM.render(
